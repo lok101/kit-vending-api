f0e0d834303e3c926e1ca696ec24276b4e654ddc
lok101 <loki97369736@gmail.com>
2026-02-05 11:27:51 +0500

    - убрал метода авторизации. Теперь данные авторизации передаются в конструктор клиента или в виде аккаунта в методы.

--- kit_api/client.py
+++ kit_api/client.py
@@ -1,6 +1,7 @@
 import hashlib
 import json
 import os
+from dataclasses import dataclass
 from datetime import datetime
 from enum import IntEnum
 from typing import Mapping, Any, Callable, Awaitable
@@ -45,6 +46,13 @@ except ValueError as e:
     )
 
 
+@dataclass(frozen=True, slots=True, kw_only=True)
+class KitAPIAccount:
+    login: str
+    password: str
+    company_id: int
+
+
 @rate_limit(max_requests, time_window)
 class KitVendingAPIClient:
 
@@ -63,18 +71,16 @@ class KitVendingAPIClient:
         self._own_session = session is None
         self._backoff = GlobalBackoff(timeout=backoff_timeout)
 
-        self._login: str | None = None
-        self._password: str | None = None
-        self._company_id: int | None = None
-
-        if login and password and company_id:
-            self.login(login, password, company_id)
+        self._login: str | None = login
+        self._password: str | None = password
+        self._company_id: int | None = company_id
 
     async def get_sales(
             self,
             from_date: datetime,
             to_date: datetime,
             vending_machine_id: int = None,
+            account: KitAPIAccount | None = None,
     ) -> SalesCollection:
         url = f"{self._base_url}/GetSales"
         to_dt_api_format = LibDateTime.datetime_to_str_kit(to_date)
@@ -92,7 +98,7 @@ class KitVendingAPIClient:
                 filter_data["Filter"]["VendingMachineId"] = vending_machine_id
 
             return {
-                "Auth": self._build_auth(request_id),
+                "Auth": self._build_auth(request_id, account),
                 **filter_data
             }
 
@@ -101,62 +107,67 @@ class KitVendingAPIClient:
 
         return sales_collection
 
-    async def get_products(self) -> ProductsKitCollection:
+    async def get_products(self, account: KitAPIAccount | None = None) -> ProductsKitCollection:
         url = f"{self._base_url}/GetGoods"
 
         async def build_data() -> dict[str, Any]:
             request_id = await self._timestamp_provider.async_get_now()
-            return {"Auth": self._build_auth(request_id)}
+            return {"Auth": self._build_auth(request_id, account)}
 
         response = await self._async_send_post_request(url, build_data)
         products_collection = ProductsKitCollection.model_validate(response)
 
         return products_collection
 
-    async def get_recipes(self) -> RecipesKitCollection:
+    async def get_recipes(self, account: KitAPIAccount | None = None) -> RecipesKitCollection:
         url = f"{self._base_url}/GetFormulations"
 
         async def build_data() -> dict[str, Any]:
             request_id = await self._timestamp_provider.async_get_now()
-            return {"Auth": self._build_auth(request_id)}
+            return {"Auth": self._build_auth(request_id, account)}
 
         response = await self._async_send_post_request(url, build_data)
         models = RecipesKitCollection.model_validate(response)
 
         return models
 
-    async def get_product_matrices(self) -> MatricesKitCollection:
+    async def get_product_matrices(self, account: KitAPIAccount | None = None) -> MatricesKitCollection:
         url = f"{self._base_url}/GetGoodsMatrices"
 
         async def build_data() -> dict[str, Any]:
             request_id = await self._timestamp_provider.async_get_now()
-            return {"Auth": self._build_auth(request_id)}
+            return {"Auth": self._build_auth(request_id, account)}
 
         response = await self._async_send_post_request(url, build_data)
         matrix_collection = MatricesKitCollection.model_validate(response)
 
         return matrix_collection
 
-    async def get_vending_machines(self) -> VendingMachinesCollection:
+    async def get_vending_machines(self, account: KitAPIAccount | None = None) -> VendingMachinesCollection:
         url = f"{self._base_url}/GetVendingMachines"
 
         async def build_data() -> dict[str, Any]:
             request_id = await self._timestamp_provider.async_get_now()
-            return {"Auth": self._build_auth(request_id)}
+            return {"Auth": self._build_auth(request_id, account)}
 
         response = await self._async_send_post_request(url, build_data)
         collection = VendingMachinesCollection.model_validate(response)
 
         return collection
 
-    async def create_matrix(self, positions: list[dict[str, Any]], matrix_name: str) -> int:
+    async def create_matrix(
+            self,
+            positions: list[dict[str, Any]],
+            matrix_name: str,
+            account: KitAPIAccount | None = None
+    ) -> int:
         url = f"{self._base_url}/CreatePiecesMatrix"
 
         async def build_data() -> dict[str, Any]:
             request_id = await self._timestamp_provider.async_get_now()
 
             return {
-                "Auth": self._build_auth(request_id),
+                "Auth": self._build_auth(request_id, account),
                 "MatrixName": matrix_name,
                 "Positions": [
                     {
@@ -172,38 +183,34 @@ class KitVendingAPIClient:
         response = await self._async_send_post_request(url, build_data)
         return int(response["Id"])
 
-    def login(self, login: str, password: str, company_id: int) -> None:
-        if not login:
-            raise KitAPIValidationError("login не может быть пустым")
-        if not password:
-            raise KitAPIValidationError("password не может быть пустым")
-        if not company_id:
-            raise KitAPIValidationError("company_id не может быть пустым")
-
-        self._login = login
-        self._password = password
-        self._company_id = company_id
-
-    def logout(self) -> None:
-        self._login = None
-        self._password = None
-        self._company_id = None
-
     def is_authenticated(self) -> bool:
         return self._login is not None and self._password is not None and self._company_id is not None
 
-    def _build_auth(self, request_id: int) -> dict[str, Any]:
-        if not self.is_authenticated():
+    def _build_auth(self, request_id: int, account: KitAPIAccount | None) -> dict[str, Any]:
+        if not self.is_authenticated() and account is None:
             raise KitAPIAuthError(
-                "Учётные данные не установлены. Используйте метод login() для установки учётных данных.")
+                "Учётные данные не установлены. Передайте данные в констуктор клиента "
+                "или в аргументах метода в виде аккаунта.")
+
+
+        if account is not None:
+            login = account.login
+            password = account.password
+            company_id = account.company_id
+
+        else:
+            login = self._login
+            password = self._password
+            company_id = self._company_id
+
 
         sign = hashlib.md5(
-            f"{self._company_id}{self._password}{request_id}".encode("utf-8")
+            f"{company_id}{password}{request_id}".encode("utf-8")
         ).hexdigest()
         return {
-            "CompanyId": self._company_id,
+            "CompanyId": company_id,
             "RequestId": request_id,
-            "UserLogin": self._login,
+            "UserLogin": login,
             "Sign": sign,
         }
 
